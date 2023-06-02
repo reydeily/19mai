47bdf8cd716966e46a23e6daca553836f8226701
reydeily <110493094+reydeily@users.noreply.github.com>
2023-06-02 08:18:28 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,49 @@
 const lista = document.querySelector("#app ul")
 const inpput=document.querySelector("#app input")
 const botao=document.querySelector("#app buttom")
+const tarefas=JSON.parse(localStorage.getItem("list"))||{}
 
-const tarefas=["estudar", "dormir"]
+const op=["estudar", "dormir"]
 
-console.log(lista, inpput, botao)
+function renderTarefas(){  
+	listElement.innerHTML = ""
+	for (const iterator of tarefas) {
+		console.log(iterator)	
+		const elementTarefa = document.createElement("li")
+		const textTarefa=document.createTextNode(iterator)
 
-for (const iterator of tarefas) {
-	const elementTarefa = document.createElement("li")
-	const textTarefa=document.createTextNode(iterator)
-	console.log(iterator)	
-	elementTarefa.appendChild(textTarefa)
-	lista.appendChild(elementTarefa)
+		const linkElement=document.createElement("a")
+		linkElement.setAttribute("href", "a")
+		const por=tarefas.indexOf(interator)
+		linkElement.setAttribute("unlock", 'deleteTarefa(${pos})')
+
+		const linktex=document.createTextNode("excluir")
+		linkElement.appendChild(linktex)
+
+		tarefaElement.appendChild(linkElement)
+
+		elementTarefa.appendChild(textTarefa)
+		listElement.appendChild(elementTarefa)
+		listElement.appendChild(elementTarefa)
+	}
+}
+renderTarefas()
+
+function addTarefas(){
+	const tarefaText = inputElement.value
+	console.log(tarefaText)
+	tarefas.push(tarefaText)
+	renderTarefas()
+	saveToStorage()
+}
+buttonElement.onclick=addTarefas
+
+function deleteTarefas(pos){
+	delete tarefas[pos]
+	renderTarefas()
+	saveToStorage()
+}
+function saveToStorage(){
+	localStorage.setItem("list", JSON.stringify(tarefas))
+
 }
